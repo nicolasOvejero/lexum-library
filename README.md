e6400232385b98433eb3304d973851a9f337d9c5
Nicolas Ovejero <nicolas.ovejero.epitech@gmail.com>
2025-09-05 11:06:27 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# lexum-library
---
+++ api/my-library/src/test/java/com/nicolas/my_library/MyLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicolas.my_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
